a9c8fcc15dbf564dac6ac16f179ec65788ca5a80
Shade-Dev <38840823+shade-sdev@users.noreply.github.com>
2021-04-08 19:43:24 +0400

    Add project files.

---
+++ SteamAppIDFinder/Finder.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using Microsoft.CSharp;
+using System.Collections.Generic;
+
+namespace SteamAppIDFinder
+{
+    public class Finder
+    {
+      
+        public static dynamic getResponse()
+        {
+            var client = new RestClient("https://api.steampowered.com/ISteamApps/GetAppList/v2/");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+            IRestResponse response = client.Execute(request);
+            dynamic jsonResponse = JsonConvert.DeserializeObject(response.Content);
+            return jsonResponse;
+        }
+
+        public static List<string[]> result(dynamic response, string search)
+        {
+            List<string[]> arrayList = new List<string[]>();
+            foreach (dynamic res in response["applist"]["apps"])
+            {
+            
+              
+                string name = res["name"];
+                if (name.Contains(search))
+                {
+
+
+                    string[] item = { res["name"], res["appid"] };
+                    arrayList.Add(item);
+                    
+
+                }
+            }
+
+            return arrayList;
+        }
+
+    }
+}
